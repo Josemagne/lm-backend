00ff613a64037580414afc3d48aca071a808a783
Josemagne <jaime-barakat@hotmail.com>
2022-05-21 15:03:27 +0200

    removed typo in auth.middleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,70 +1,81 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from "jsonwebtoken"
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import UnauthenticatedError from '../errors/unauthenticated.error';
-import { join } from 'path';
-dotenv.config()
+import UnauthenticatedError from "../errors/unauthenticated.error";
+import { join } from "path";
+dotenv.config();
 
 // @ts-ignore
-const authenticationMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+const authenticationMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   let decoded: any;
   let token: any;
-    const authHeader = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-    // TODO Must have a token after Bearer
-// @ts-ignore
-    if (!authHeader || !authHeader.startsWith('Bearer ') && new RegExp("\w{21,}").exec(authHeader.split(' ')[1])) {
-        return res.status(401).send("No token provided");
+  // TODO Must have a token after Bearer
+  // @ts-ignore
+  if (
+    !authHeader ||
+    (!authHeader.startsWith("Bearer ") &&
+      new RegExp("w{21,}").exec(authHeader.split(" ")[1]))
+  ) {
+    return res.status(401).send("No token provided");
+  }
+
+  // Get the token
+  token = authHeader.split(" ")[1];
+
+  try {
+    if (!process.env.JWT_SECRET) {
+      throw new UnauthenticatedError("process.env.JWT_SECRET is not given");
     }
 
-    // Get the token
-    token = authHeader.split(' ')[1];
-
-    try {
-
+    // Decode token
+    // Throws an err if expired
+    console.log("last log:");
+    decoded = await jwt.verify(
+      token,
+      process.env.JWT_SECRET,
+      (err, decoded) => {
         if (!process.env.JWT_SECRET) {
-            throw new UnauthenticatedError("process.env.JWT_SECRET is not given");
+          throw new UnauthenticatedError("process.env.JWT_SECRET is not given");
+        }
+        // If the token is expired
+        if (decoded.exp * 1000 < Date.now()) {
+          token = jwt.sign(
+            { email: email, user_id: user_id },
+            process.env.JWT_SECRET,
+            {
+              expiresIn: "30d",
+            }
+          );
         }
 
-        // Decode token
-      // Throws an err if expired
-      console.log("last log:")
-      decoded = await jwt.verify(token, process.env.JWT_SECRET, (err, decoed) => {
-        if (!process.env.JWT_SECRET) {
-            throw new UnauthenticatedError("process.env.JWT_SECRET is not given");
-        }
-      // If the token is expired
-      if (decoded.exp * 1000 < Date.now()) {
-      token = jwt.sign({ email: email, user_id: user_id}, process.env.JWT_SECRET, {
-          expiresIn: '30d'
-      })
-        
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
+        return decoded;
       }
+    );
+    // @ts-ignore
+    const { user_id, email } = decoded;
 
-      return decoded = jwt.verify(token, process.env.JWT_SECRET)
+    // @ts-ignore
+    res.locals.user = { user_id, email };
 
-      })
-        // @ts-ignore
-        const { user_id, email } = decoded;
+    // Go to controller
+    next();
+  } catch (err) {
+    console.log(err);
 
-        // @ts-ignore
-        res.locals.user = { user_id, email }
+    const { user_id, email } = decoded;
+    // @ts-ignore
+    res.locals.user = { user_id, email };
 
-        // Go to controller
-        next();
-    }
+    // Go to controller
+    next();
+  }
+};
 
-    catch (err) {
-        console.log(err);
-
-        const { user_id, email } = decoded;
-        // @ts-ignore
-        res.locals.user = { user_id, email }
-
-        // Go to controller
-        next();
-    }
-
-}
-
-export { authenticationMiddleware }
+export { authenticationMiddleware };
